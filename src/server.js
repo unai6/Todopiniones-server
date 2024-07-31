a887416e29181a4282b631edcc7b6db7e7347b80
Unai Gonzalez <ugonzalez@dogfydiet.com>
2024-07-31 18:08:38 +0200

    Add app import inside function start

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
-const fastifyApp = require('./app')
 
 async function start () {
+  const fastifyApp = require('./app')
+
   await fastifyApp.listen({ port: process.env.PORT || 80 })
 
   fastifyApp.ready(() => {
-    console.info('\n' + fastifyApp.printRoutes({ commonPrefix: false }))
+    if (process.env.NODE_ENV === 'development') {
+      fastifyApp.log.info('\n' + fastifyApp.printRoutes({ commonPrefix: false }))
+    }
+
     console.info(
       '\n=======================================================' +
       `\nDogfy API ready / Port: ${fastifyApp.server.address().port} / NODE_ENV: ${process.env.NODE_ENV}` +
