050b54b86043e27d5a4978f7c053dede86d241e2
Unai Gonzalez <ugonzalez@dogfydiet.com>
2024-07-31 18:23:22 +0200

    Add host and yaml

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 
 async function start () {
   const fastifyApp = require('./app')
+  const host = ("RENDER" in process.env) ? `0.0.0.0` : 'localhost'
 
-  await fastifyApp.listen({ port: process.env.PORT || 80 })
+  await fastifyApp.listen({ host, port: process.env.PORT || 80, })
 
   fastifyApp.ready(() => {
     if (process.env.NODE_ENV === 'development') {
