cc831315c0a5845029dc10e62518b74d5d32994c
Unai Gonzalez <ugonzalez@dogfydiet.com>
2024-08-07 17:21:47 +0200

    Update scraping function to get amazon link id

--- src/api/products/products.handlers.js
+++ src/api/products/products.handlers.js
@@ -41,32 +41,64 @@ async function getProducts(req, reply) {
     const $ = cheerio.load(html)
     const products = []
 
-    $('.s-widget-container').each((i, element) => {
-      const titleElement = $(element).find('.s-title-instructions-style')
-      const priceElement = $(element).find('.a-price > span').first()
-      const priceWholeElement = $(element).find('.a-price-whole')
-      const imageElement = $(element).find('.s-image')
-      const ratingElement = $(element).find('.a-icon-alt')
-      const hrefElement = $(element).find('.a-link-normal')
+//     $('.s-widget-container').each((i, element) => {
+//       const titleElement = $(element).find('.s-title-instructions-style')
+//       const priceElement = $(element).find('.a-price > span').first()
+//       const priceWholeElement = $(element).find('.a-price-whole')
+//       const imageElement = $(element).find('.s-image')
+//       const ratingElement = $(element).find('.a-icon-alt')
+//       const hrefElement = $(element).find('.a-link-normal')
+//
+//       const title = titleElement.text()
+//       const price = parseInt(priceElement.text().replace(/[$,]/g, ""), 10)
+//       const priceWhole = parseFloat(priceWholeElement.text().replace('.', ''))
+//       const src = imageElement.attr('src')
+//       const rating = ratingElement.text()
+//       const href = getProductReferralLink(hrefElement.attr('href'))
+//
+//       if (!title || isNaN(price) || isNaN(priceWhole)) return
+//
+//       products.push({
+//         title,
+//         priceWhole: Number(priceWhole),
+//         price: priceElement.text(),
+//         src,
+//         rating,
+//         href,
+//       })
+    //     })
 
-      const title = titleElement.text()
-      const price = parseInt(priceElement.text().replace(/[$,]/g, ""), 10)
-      const priceWhole = parseFloat(priceWholeElement.text().replace('.', ''))
-      const src = imageElement.attr('src')
-      const rating = ratingElement.text()
-      const href = hrefElement.attr('href')
+  const elements = $('.s-widget-container').toArray()
 
-      if (!title || isNaN(price) || isNaN(priceWhole)) return
+    for (const element of elements) {
+        const titleElement = $(element).find('.s-title-instructions-style')
+        const priceElement = $(element).find('.a-price > span').first()
+        const priceWholeElement = $(element).find('.a-price-whole')
+        const imageElement = $(element).find('.s-image')
+        const ratingElement = $(element).find('.a-icon-alt')
+        const hrefElement = $(element).find('.a-link-normal')
 
-      products.push({
-        title,
-        priceWhole: Number(priceWhole),
-        price: priceElement.text(),
-        src,
-        rating,
-        href,
-      })
-    })
+        const title = titleElement.text()
+        const price = parseInt(priceElement.text().replace(/[$,]/g, ""), 10)
+        const priceWhole = parseFloat(priceWholeElement.text())
+
+      if (priceWhole > max || priceWhole < min || isNaN(price) || isNaN(priceWhole)) continue
+
+        const src = imageElement.attr('src')
+        const rating = ratingElement.text()
+        const href = await getProductReferralLink(hrefElement.attr('href'))
+
+        if (!title) continue
+
+        products.push({
+          title,
+          priceWhole: Number(priceWhole),
+          price: priceElement.text(),
+          src,
+          rating,
+          href,
+        })
+    }
 
     const formattedProducts = products.filter(el => el.priceWhole <= max && el.priceWhole > min).sort((p1, p2) => p1.priceWhole - p2.priceWhole) || []
 
@@ -86,24 +118,57 @@ async function getProducts(req, reply) {
 
 // ---------
 async function getProductReferralLink (productUrl) {
-  const { data: html } = await axios.get(`https://www.amazon.es/${productUrl}`, {
-      headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-      'Accept-Language': 'en-US,en;q=0.9',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Referer': 'https://www.amazon.es/',
-      'Connection': 'keep-alive',
-      }
-  })
+  if (!productUrl || productUrl ===  '/gp/help/customer/display.html?nodeId=200533820') return
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
 
-  const $ = cheerio.load(html)
+  // Go to Amazon sign-in page
+  await page.goto('https://www.amazon.es/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fafiliados.amazon.es%2F&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_associates_es&openid.mode=checkid_setup&marketPlaceId=A1RKKUPIHCS9HS&language=es_ES&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0')
+  // Enter email
+  await page.waitForSelector('input[name="email"]')
+  await page.type('input[name="email"]', 'unaigoe91@gmail.com')
+  await page.evaluate(() =>  new Promise(resolve => setTimeout(resolve, Math.random() * 2000 + 1000)))
 
-  const link = $('.amzn-ss-link-container')
-  console.info(link.html())
+  // Enter password
+  const inputContinue = await page.$('input#continue')
+  if (!inputContinue) {
+    await page.type('input[name="password"]', '45788916Xx')
+    await page.evaluate(() =>  new Promise(resolve => setTimeout(resolve, Math.random() * 2000 + 1000)))
+  } else {
+    await page.click('input#continue')
+    await page.waitForNavigation()
+    await page.evaluate(() => new Promise(resolve => setTimeout(resolve, Math.random() * 2000 + 1000)))
+
+    await page.type('input[name="password"]', '45788916Xx')
+    await page.evaluate(() =>  new Promise(resolve => setTimeout(resolve, Math.random() * 2000 + 1000)))
+  }
+
+  // Click sign-in button
+  await page.click('input#signInSubmit')
+  await page.waitForNavigation()
+  await page.evaluate(() =>  new Promise(resolve => setTimeout(resolve, Math.random() * 2000 + 1000)))
+
+  // Go to the product URL
+  await page.goto(`https://amazon.es${productUrl}`, { waitUntil: 'networkidle2' })
+
+  await page.evaluate(() =>  new Promise(resolve => setTimeout(resolve, Math.random() * 2000 + 1000)))
+  // Extract the smart link
+  await page.waitForSelector('textarea#amzn-ss-text-shortlink-textarea')
+  // const smartlink = await page.$eval('li#amzn-ss-text-link', el => el.textContent)
+  await page.click('li#amzn-ss-text-link')
+  await page.evaluate(() =>  new Promise(resolve => setTimeout(resolve, Math.random() * 2000 + 1000)))
+
+  const smartLink = await page.$eval('textarea#amzn-ss-text-shortlink-textarea', el => el.value)
+
+  await browser.close()
+
+  console.info(smartLink, 'smartLink')
+
+  return smartLink
 }
 
 module.exports = {
   getProducts,
   getScrapedProducts,
+  getProductReferralLink,
 }
